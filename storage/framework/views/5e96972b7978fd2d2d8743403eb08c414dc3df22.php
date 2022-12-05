520be1a4d4bceff28a0c4dc72715f8d7066d2736
Kevin Almer <kevinalmer4@gmail.com>
2022-12-05 08:50:42 +0700

    ubah main branch

---
+++ storage/framework/views/5e96972b7978fd2d2d8743403eb08c414dc3df22.php
@@ -0,0 +1,24 @@
+
+<?php $__env->startSection('con'); ?>
+    
+        <!-- Jumbotron -->
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <p class="p-the-where">
+                    <span class="the-where"><i class="fa-solid fa-location-dot"></i> Masjid At-Taqwa, Cibinong city, Bogor</span>
+                </p>
+                <h1>
+                    A Relialble islamic <span>center</span><br />
+                    to follow quran & sunnah
+                </h1>
+                <p>The Salat is the time when the meeting with allah and <br />the ascension (Mi'raj) Of the believer takes places</p>
+                <a href="" class="btn btn-success tombol">Tour</a>
+            </div>
+        </div>
+        <!-- akhir jumbotron -->
+
+       </body>
+</html>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.main', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\application\projek\resources\views/index.blade.php ENDPATH**/ ?>
